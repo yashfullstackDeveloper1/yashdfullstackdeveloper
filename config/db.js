7a6232c03f916d468bec475fe60bdc9fabed0f23
yashfullstackDeveloper1 <yashdhapke6@gmail.com>
2026-05-16 14:00:58 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://scos_db_xt9u_user:BVZsQs7AAG0YwiXGvwknIJ2z9XO4wRKz@dpg-d841c5jtqb8s73et9rl0-a.oregon-postgres.render.com/scos_db_xt9u',
-  ssl: true
+  connectionString: 'postgresql://scos_db_xt9u_user:BVZsQs7AAG0YwiXGvwknIJ2z9XO4wRKz@dpg-d841c5jtqb8s73et9r10-a.ohio-postgres.render.com/scos_db_xt9u',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
