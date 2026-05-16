f9458e3de55f337a60fc550ca5bfa3004e0ca1f5
yashfullstackDeveloper1 <yashdhapke6@gmail.com>
2026-05-16 13:25:27 +0530

    Refactor database connection to use DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -1,21 +1,15 @@
-// Keep some database connections ready so server responds faster!
 const { Pool } = require('pg');
-// Load .env file values so we can use DB_HOST, DB_PASSWORD etc
 require('dotenv').config();
 
-// Create a connection to PostgreSQL using details from .env file
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// if connect print 1 if not then print 2 line error code
 pool.connect()
-  .then(() => console.log('Database connected successfully'))
-  .catch((err) => console.error('Database connection failed:', err));
+  .then(() => console.log(' Database connected successfully'))
+  .catch((err) => console.error(' Database connection failed:', err));
 
-// Share this pool with other files so they can also use the database connection
 module.exports = pool;
